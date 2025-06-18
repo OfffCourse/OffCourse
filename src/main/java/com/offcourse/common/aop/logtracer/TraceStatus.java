ef94584c41898ff24ee53af106b08e7bcb1541bd
heebum kim <heebum9955@naver.com>
2025-06-18 19:57:08 +0900

    feat: Logging AOP 구현 Log4j 활용

---
+++ src/main/java/com/offcourse/common/aop/logtracer/TraceStatus.java
@@ -0,0 +1,12 @@
+package com.offcourse.common.aop.logtracer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimesMs;
+    private String message;
+}
