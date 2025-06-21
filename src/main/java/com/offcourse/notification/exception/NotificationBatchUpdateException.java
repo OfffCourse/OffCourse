979a89d9e74bfe2e3c9f7b0ef693a1189fd772e9
heebum kim <heebum9955@naver.com>
2025-06-21 22:32:45 +0900

    feat: Notification Custom Exception 구현

---
+++ src/main/java/com/offcourse/notification/exception/NotificationBatchUpdateException.java
@@ -0,0 +1,7 @@
+package com.offcourse.notification.exception;
+
+public class NotificationBatchUpdateException extends RuntimeException {
+    public NotificationBatchUpdateException(String message) {
+        super(message);
+    }
+}
