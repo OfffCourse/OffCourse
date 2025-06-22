333ac0b423b22d7066b3018fda12b462f532ecc8
sysypp <parkseoyoung0915@gmail.com>
2025-06-22 19:06:08 +0900

    feat: 마이페이지용 DTO 구현

---
+++ src/main/java/com/offcourse/mypage/model/dto/TeacherMyPageResponse.java
@@ -0,0 +1,30 @@
+package com.offcourse.mypage.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TeacherMyPageResponse {
+    private Long courseSeq;
+    private String courseName;
+    private String courseCurriculum;
+    private Date courseStartDate;
+    private Date courseEndDate;
+    private String courseAddress;
+    private String courseDetailAddress;
+    private Integer coursePrice;
+    private Integer courseDiscount;
+    private String courseQaLink;
+    private Integer reviewCount;
+    private Double averageRating;
+    private Integer courseSize;
+    private Integer courseCurrentSize;
+    private Double accountPrice;
+}
