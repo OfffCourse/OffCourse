f1caeb60fd1db669509a2001db7d760cca3e7652
sysypp <parkseoyoung0915@gmail.com>
2025-06-24 00:35:52 +0900

    feat: 리뷰 DTO 구현

--- src/main/java/com/offcourse/course/model/dto/Review.java
+++ src/main/java/com/offcourse/course/model/dto/Review.java
@@ -1,10 +1,8 @@
 package com.offcourse.course.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
--- src/main/java/com/offcourse/course/model/dto/ReviewViewResponse.java
+++ src/main/java/com/offcourse/course/model/dto/ReviewViewResponse.java
@@ -1,12 +1,10 @@
 package com.offcourse.course.model.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.sql.Timestamp;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
