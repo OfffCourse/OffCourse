b614934fd9cc3ab5b7e9fc5fc5326d6e6ba34dd5
sysypp <parkseoyoung0915@gmail.com>
2025-06-24 16:24:04 +0900

    feat: 첨부파일 DTO 구현

---
+++ src/main/java/com/offcourse/course/model/dto/AttachmentViewResponse.java
@@ -0,0 +1,21 @@
+package com.offcourse.course.model.dto;
+
+import lombok.*;
+
+import java.sql.Date;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AttachmentViewResponse {
+    private Long attSeq;
+    private String attOriName;
+    private String attRenamedName;
+    private String attType;
+    private Long episodeSeq;
+    private int episodeCount;
+    private Date episodeDate;
+}
+
