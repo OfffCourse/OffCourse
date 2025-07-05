fa5bc77a5efa1cf7c8eae0004462b5e37d43064a
heebum kim <heebum9955@naver.com>
2025-07-05 16:59:58 +0900

    fix: 배포용으로 코드수정

--- src/main/java/com/offcourse/redis/scheduler/QueueScheduler.java
+++ src/main/java/com/offcourse/redis/scheduler/QueueScheduler.java
@@ -6,11 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 import java.util.Set;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 @Slf4j
 public class QueueScheduler {
